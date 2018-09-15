c740d6cdf12e582ec5748eb7cbf8b630d8267107
Kiryl Zavarotny <kirillzavorotni@gmail.com>
2018-09-15 15:56:46 +0300

    created class Sorted, added constructor, added following function: add, at, toArray sort, get length

--- src/index.js
+++ src/index.js
@@ -1,31 +1,50 @@
 class Sorter {
-  constructor() {
-    // your implementation
-  }
+    constructor() {
+        this.storage = [];
+        this.sortItems = [];
+    }
 
-  add(element) {
-    // your implementation
-  }
+    add(element) {
+        this.storage.push(element);
+    }
 
-  at(index) {
-    // your implementation
-  }
+    at(index) {
+        return this.storage[index];
+    }
 
-  get length() {
-    // your implementation
-  }
+    get length() {
+        return this.storage.length;
+    }
 
-  toArray() {
-    // your implementation
-  }
+    toArray() {
+        return this.storage;
+    }
 
-  sort(indices) {
-    // your implementation
-  }
+    sort(indices) {
+        let sort_item = this.sortItems;
+        let storage = this.storage;
 
-  setComparator(compareFunction) {
-    // your implementation
-  }
+        indices.forEach(function(index){
+            sort_item.push(storage[index]);
+            storage.splice(index, 1, true);
+        });
+
+        sort_item.sort(function(a,b){
+            return a - b;
+        });
+
+        for(let i = 0; i < storage.length; i++){
+            if(storage[i] === true){
+                storage[i] = sort_item[0];
+                sort_item.splice(0, 1);
+            }
+        }
+        return storage;
+    }
+
+    setComparator(compareFunction) {
+        // your implementation
+    }
 }
 
 module.exports = Sorter;
